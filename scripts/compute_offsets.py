8af7dc4ae7e0e7d82c6d9b545def81198827cc37
janabel <janabel@mit.edu>
2023-03-31 17:59:00 +0700

    fix register reduce scripts

--- scripts/compute_offsets.py
+++ scripts/compute_offsets.py
@@ -4,17 +4,23 @@ import sys
 
 return_str = ""
 
-return_str += "PrimeReduce7Registers: \n"
-for idx in range(4, 7):
-    coeff = 2**(64*idx) % compute_p256_math.P
-    long_coeff = compute_p256_math.get_long(64, 4, coeff)
-    return_str += "in[" + str(idx) + "], coeffs = " + str(long_coeff) + '\n'
 
-return_str += "\n PrimeReduce10Registers: \n"
-for idx in range(10):
-    coeff = 2**(64*idx) % compute_p256_math.P
-    long_coeff = compute_p256_math.get_long(32, 8, coeff)
-    return_str += "in[" + str(idx) + "], coeffs = " + str(long_coeff) + '\n'
+def offsets(num_registers, n, k):
+    return_str = "PrimeReduce" + str(num_registers) + "Registers: \n"
+    matrix = []
+    for idx in range(num_registers):
+        coeff = 2**(64*idx) % compute_p256_math.P
+        long_coeff = compute_p256_math.get_long(n, k, coeff)
+        matrix.append(long_coeff)
+        return_str += "in[" + str(idx) + "], coeffs = " + \
+            str(long_coeff) + '\n'
+    return_str += "\n"
+    return_str += "matrix of coefficients = " + str(matrix) + "\n" + "\n"
+
+    return return_str
+
+
+return_str += offsets(7, 64, 4) + offsets(10, 32, 8)
 
 orig_stdout = sys.stdout
 f = open('../script_outputs/offsets_out.circom', 'w')
